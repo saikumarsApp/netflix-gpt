0789b78f782b8518f697a88e3c0eb0cfb0420532
saikumarsApp <saikumarnp8@gmail.com>
2025-08-22 23:18:35 +0530

    removing extra files

--- src/utils/geminiAI.js
+++
@@ -1,7 +0,0 @@
-import { GoogleGenAI } from "@google/genai";
-
-const ai = new GoogleGenAI(process.env.REACT_APP_GEMINI_KEY);
-const response = await ai.models.generateContent({
-  model: "gemini-2.5-flash",
-  contents: "Explain how AI works in a few words",
-});
