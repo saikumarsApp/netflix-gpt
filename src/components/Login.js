86f90bab9e9d68f7f2b88966d5ea0e410f77d5c5
saikumarsApp <saikumarnp8@gmail.com>
2025-08-22 23:48:55 +0530

    solution

--- src/components/Login.js
+++ src/components/Login.js
@@ -64,66 +64,71 @@ const Login = () => {
       <Header />
       <div className="absolute">
         <img
+          className="h-screen w-screen object-cover"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/a927b1ee-784d-494a-aa80-cf7a062d2523/web/IN-en-20250714-TRIFECTA-perspective_5acb7337-c372-45ec-ae12-ddb110e6ad78_large.jpg"
           alt="body"
         />
       </div>
-      <form
-        onClick={(e) => e.preventDefault()}
-        className="absolute rounded-lg w-[30%] my-36 mx-auto right-0 left-0 p-12 bg-black text-white bg-opacity-90"
-      >
-        <h1 className="font-bold text-2xl my-2">
-          {isSignIn ? "Sign In" : "Sign Up"}
-        </h1>
-        {!isSignIn && (
+      <div className="flex justify-center">
+        <form
+          onClick={(e) => e.preventDefault()}
+          className="absolute rounded-lg mt-20 w-[90%]  md:w-[30%] md:my-36 mx-auto md:right-0 md:left-0 p-12 bg-black text-white bg-opacity-90"
+        >
+          <h1 className="font-bold text-2xl my-2">
+            {isSignIn ? "Sign In" : "Sign Up"}
+          </h1>
+          {!isSignIn && (
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="p-4 my-2 w-[90%] rounded-sm bg-gray-700 outline-none bg-opacity-50"
+            />
+          )}
+
           <input
             type="text"
-            placeholder="Full Name"
+            ref={email}
+            placeholder="Email or Mobile Number"
             className="p-4 my-2 w-[90%] rounded-sm bg-gray-700 outline-none bg-opacity-50"
           />
-        )}
-
-        <input
-          type="text"
-          ref={email}
-          placeholder="Email or Mobile Number"
-          className="p-4 my-2 w-[90%] rounded-sm bg-gray-700 outline-none bg-opacity-50"
-        />
-        <input
-          type="password"
-          ref={password}
-          placeholder="password"
-          className="p-4 my-2 w-[90%] rounded-sm bg-gray-700 outline-none bg-opacity-50"
-        />
-        <p className="text-red-500 font-bold text-sm my-2">{errorMessage}</p>
-        <button
-          onClick={handleValidation}
-          className="bg-red-600 my-2 font-bold py-2 px-5 w-[90%] rounded-sm text-lg"
-        >
-          {isSignIn ? "Sign In" : "Sign Up"}
-        </button>
-        <p className="text-center my-2 text-gray-500 font-semibold my-2">OR</p>
-        <button className="bg-gray-500 bg-opacity-50  rounded-sm font-bold py-2 px-5 w-[90%] ">
-          Use a sign-in code
-        </button>
-        <p className="cursor-pointer text-center my-2 underline">
-          Forgot password?
-        </p>
-
-        <div className="flex items-center">
-          <input type="checkbox" className="w-5 h-5 mr-3 my-3" />
-          <label className="text-sm">Remember Me</label>
-        </div>
-        <p className="text-sm text-gray-200">
-          {isSignIn ? "New to Netflix?" : "Alredy registred!"}
-          <span
-            onClick={toggleSignInForm}
-            className="hover:underline cursor-pointer font-bold"
+          <input
+            type="password"
+            ref={password}
+            placeholder="password"
+            className="p-4 my-2 w-[90%] rounded-sm bg-gray-700 outline-none bg-opacity-50"
+          />
+          <p className="text-red-500 font-bold text-sm my-2">{errorMessage}</p>
+          <button
+            onClick={handleValidation}
+            className="bg-red-600 my-2 font-bold py-2 px-5 w-[90%] rounded-sm text-lg"
           >
-            {isSignIn ? "Sign Up" : "Sign In"}
-          </span>
-        </p>
-      </form>
+            {isSignIn ? "Sign In" : "Sign Up"}
+          </button>
+          <p className="text-center my-2 text-gray-500 font-semibold my-2">
+            OR
+          </p>
+          <button className="bg-gray-500 bg-opacity-50  rounded-sm font-bold py-2 px-5 w-[90%] ">
+            Use a sign-in code
+          </button>
+          <p className="cursor-pointer text-center my-2 underline">
+            Forgot password?
+          </p>
+
+          <div className="flex items-center">
+            <input type="checkbox" className="w-5 h-5 mr-3 my-3" />
+            <label className="text-sm">Remember Me</label>
+          </div>
+          <p className="text-sm text-gray-200">
+            {isSignIn ? "New to Netflix?" : "Alredy registred!"}
+            <span
+              onClick={toggleSignInForm}
+              className="hover:underline cursor-pointer font-bold"
+            >
+              {isSignIn ? "Sign Up" : "Sign In"}
+            </span>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
